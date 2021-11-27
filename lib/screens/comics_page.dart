174c6d4b27b6c8ec7be634ff38f0775989a79807
Diego Martinez <juandiego337@hotmail.com>
2021-11-27 15:39:18 -0500

    A grid view is implemented to show the cards of the comics

--- lib/screens/comics_page.dart
+++ lib/screens/comics_page.dart
@@ -5,10 +5,52 @@ class ComicsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Comics page'),
-      ),
+    return  Scaffold(
+      body: GridView.builder(
+        padding: const EdgeInsets.only(top: 20),
+        itemCount: 10, //TODO. comics.length
+        gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+          mainAxisExtent: 210, // here set custom Height You Want
+        ),
+        itemBuilder: (BuildContext context , int index){
+          return const CardComic();
+        })
     );
   }
 }
+
+
+class CardComic extends StatelessWidget {
+  const CardComic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: FadeInImage.assetNetwork(
+                width: double.infinity,
+                height: 150,
+                fit: BoxFit.cover,
+                placeholder: 'assets/spinning-loading.gif',
+                image: 'https://via.placeholder.com/200x300'
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text('Title.comic',
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            )
+          ],
+        ),
+      );
+  }
+}
+
